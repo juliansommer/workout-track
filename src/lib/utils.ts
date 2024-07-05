8bdcee99e70089957fafd0bb5098ce08cd7200df
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-07-05 13:23:45 +1000

    actually fix env var

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getURL() {
   let url =
-    process?.env?.VERCEL_PROJECT_PRODUCTION_URL ?? // Automatically set by Vercel.
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
     "http://localhost:3000/"
   // Make sure to include `https://` when not localhost.
