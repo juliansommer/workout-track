d47a250918bf02ec8a06463e213c2ed4b9ec35f7
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-07-28 17:11:26 +1000

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,8 @@ import type { Config } from "tailwindcss"
 import { fontFamily } from "tailwindcss/defaultTheme"
 
 export default {
-  darkMode: ["class"],
   content: ["./src/**/*.{ts,tsx}"],
+  darkMode: ["class"],
   theme: {
     container: {
       center: true,
