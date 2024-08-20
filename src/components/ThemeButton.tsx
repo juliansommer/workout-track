9ff29b664addb6aafcf42fd597212849a9d0fd18
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-08-20 14:35:11 +1000

    themebutton hydration fix

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -2,9 +2,20 @@
 import { Button } from "@/components/ui/Button"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
+import { useEffect, useState } from "react"
 
 export default function ThemeButton() {
+  const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
 
   return (
     <Button
