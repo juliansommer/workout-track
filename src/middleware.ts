dc67d21931942137c0aa2d6b9805b8f1834e62a4
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-08-20 14:00:34 +1000

    fix middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,5 +6,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/exercises/:path", "/plans/:path*"],
+  matcher: ["/exercises/:path*", "/plans/:path*"],
 }
