94b6438e820d8b63239fc0c67897d1f9e8e8ec8c
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-08-23 12:13:23 +1000

    tailwind fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,6 @@ const config: Config = {
   content: ["./src/**/*.{ts,tsx}"],
   darkMode: ["class"],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
