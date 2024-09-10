66e69fec91b022f2511789fe64bce642270fc1db
jsommer6 <115516918+jsommer6@users.noreply.github.com>
2024-09-10 13:40:43 +1000

    tailwind fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss"
 import { fontFamily } from "tailwindcss/defaultTheme"
 
 const config: Config = {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: ["./src/**/*.tsx"],
   darkMode: ["class"],
   theme: {
     extend: {
