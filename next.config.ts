6e41d7b42a8115ce2986b8e5ece4d2b26fb1ee77
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2025-05-30 16:23:00 +1000

    add a CSP

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,34 @@ import type { NextConfig } from "next"
 
 import "./src/env"
 
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data:;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  upgrade-insecure-requests;
+`
+
 const nextConfig: NextConfig = {
   poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
