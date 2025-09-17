9e7a30b902d0ac35bddbd74bd4624c6a29541d28
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2025-09-17 10:36:19 +1000

    use prettier.config.ts

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("prettier").Config & import("prettier-plugin-tailwindcss").PluginOptions} */
-const config = {
-  bracketSameLine: false,
-  semi: false,
-  plugins: ["prettier-plugin-tailwindcss"],
-}
-
-export default config
---
+++ prettier.config.ts
@@ -0,0 +1,9 @@
+import { type Config } from "prettier"
+
+const config: Config = {
+  bracketSameLine: false,
+  semi: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+}
+
+export default config
