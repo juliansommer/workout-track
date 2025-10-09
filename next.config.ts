8e1844aa0ed63f45064efd0e69d0e5a32dafbdd2
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2025-10-10 10:31:48 +1100

    fix csp

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,7 @@ const cspHeader = `
   base-uri 'self';
   form-action 'self';
   frame-ancestors 'none';
+  connect-src 'self' https://nsfllbqyozduniyhtfpa.supabase.co;
   upgrade-insecure-requests;
 `
 
